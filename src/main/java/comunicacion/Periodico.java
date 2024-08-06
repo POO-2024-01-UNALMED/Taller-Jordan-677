c854e2b60ce2a64e9bd64d7d586a2c41237332f2
Jordan-677 <159493664+Jordan-677@users.noreply.github.com>
2024-08-05 23:13:52 -0500

    intento 1
    primer intento codigo completo

---
+++ src/main/java/comunicacion/Periodico.java
@@ -0,0 +1,28 @@
+package comunicacion;
+
+public class Periodico extends Escrito {
+
+	public Periodico(String origen, String titulo, String autor, int paginas) {
+		super(origen, titulo, autor, paginas);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	int palabrasTotales(int palabrasPagina) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	String interpretacion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
